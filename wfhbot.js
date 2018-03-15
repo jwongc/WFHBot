dd03ad3ca97ea5a1dc7d2620d6b80cf99f7d2357
Jeffrey Wong <jeffreywongc@yahoo.com>
2018-03-15 16:00:17 -0700

    Something's working now

--- wfhbot.js
+++ wfhbot.js
@@ -1,14 +1,13 @@
-const RtmClient  = require('@slack/client').RtmClient;
-const WebClient  = require('@slack/client').WebClient;
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const { RTMClient, WebClient } = require('@slack/client');
 
-const bot_token = 'SLACKTOKEN';
-const rtm       = new RtmClient(bot_token);
+const bot_token = 'xoxb-330103983009-IqyyUITJ7pevYnN9GBeGO6SL';
+const rtm       = new RTMClient(bot_token);
 const web       = new WebClient(bot_token);
 
 const robotName   = 'wfhbot';
 
 let users = [];
+let wfhUsers = [];
 
 function executeCommand(command, args) {
     console.log(command, args);
@@ -23,31 +22,6 @@ function getUsernameFromId(id) {
     return user ? user.name : 'unknown member';
 }
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-    if (message.type === 'message' && message.text) {
-        const userName = getUsernameFromId(message.user);
-        if (userName !== robotName) {
-            if (message.text.trim().toLowerCase() === '!wfh help') {
-                rtm.sendMessage('Hey ' + userName + '! To add yourself to the WFH list, simply enter !wfh. To get the WFH list, enter !wfhlist', message.channel);
-            }
-            if (message.text.trim().toLowercase() === '!wfhlist') {
-                var returnMessage = 'Users that are working from home: ';
-                users.forEach(function(userName, index) {
-                    returnMessage.concat(userName);
-                    if (index > users.length - 1) {
-                        returnMessage.concat(', ');
-                    }
-                });
-                rtm.sendMessage(returnMessage);
-            }
-            if (message.text.toLowerCase().trim().toLowerCase() === ('!wfh')) {
-                users.push(userName);
-                rtm.sendMessage('Added ' + userName + ' to WFH list.', message.channel);
-            }
-        }
-    }
-});
-
 web.users.list((err, data) => {
     if (err) {
         console.error('web.users.list Error:', err);
@@ -57,3 +31,29 @@ web.users.list((err, data) => {
 });
 
 rtm.start();
+
+rtm.on('message', function handleRtmMessage(message) {
+    if (message.text) {
+        const userName = getUsernameFromId(message.user);
+        if (userName !== robotName) {
+            if (message.text.trim().toLowerCase() === '!wfh help') {
+                rtm.sendMessage('Hey ' + userName + '! To add yourself to the WFH list, simply enter !wfh. To get the WFH list, enter !wfhlist', message.channel);
+            }
+            if (message.text.trim().toLowerCase() === '!wfhlist') {
+                var returnMessage = 'Users that are working from home: ';
+                var userCount = wfhUsers.length;
+                wfhUsers.forEach(function(userName, index) {
+                    returnMessage = returnMessage + userName;
+                    if (index !== userCount - 1) {
+                        returnMessage = returnMessage + ", ";
+                    }
+                });
+                rtm.sendMessage(returnMessage, message.channel);
+            }
+            if (message.text.toLowerCase().trim().toLowerCase() === ('!wfh')) {
+                wfhUsers.push(userName);
+                rtm.sendMessage('Added ' + userName + ' to WFH list.', message.channel);
+            }
+        }
+    }
+});
